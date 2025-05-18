a5f175ac4a938c5219648967a7676631ea5e7622
Pavel Petrov <doctorrosen2016@gmail.com>
2025-05-18 23:12:17 +0600

    asdasdasd

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const QRCode = require('qrcode');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.get('/generate', async (req, res) => {
     const url = req.query.url;
